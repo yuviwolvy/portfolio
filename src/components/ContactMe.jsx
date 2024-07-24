7b8317d8edd97a68d3eefef9a40c77ee8766ca2c
Yuvraj <logindevendrasinhchauhan@gmail.com>
2024-07-24 23:35:51 +0530

    Close to comlete contactMe

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,14 +1,20 @@
 import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare";
+import { Instagram } from "@styled-icons/bootstrap/Instagram";
+import { Github } from "@styled-icons/boxicons-logos/Github";
+import { Leetcode } from "styled-icons/simple-icons";
+import { ArrowDownCircleFill } from "styled-icons/bootstrap";
 
 const ContactMe = () => {
   const form = useRef();
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    from_name: "",
+    from_email: "",
     message: "",
   });
-  const [acknowledgment, setAcknowledgment] = useState("");
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -18,61 +24,43 @@ const ContactMe = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm("service_34zfppd", "template_br26u9k", form.current, {
-        publicKey: "dfuT-0kdmn4wYwm2g",
-      })
+      .sendForm(
+        "service_34zfppd",
+        "template_br26u9k",
+        e.target,
+        "dfuT-0kdmn4wYwm2g"
+      )
       .then(
         (response) => {
           console.log("SUCCESS!", response.status, response.text);
-          setAcknowledgment(
+          toast.success(
             "Thank you for contacting me! I will get back to you soon."
           );
         },
         (error) => {
           console.log("FAILED...", error);
-          setAcknowledgment("Sorry, something went wrong. Please try again.");
+          toast.error("Sorry, something went wrong. Please try again.");
         }
       );
 
-    // emailjs
-    //   .send(
-    //     "service_34zfppd",
-    //     "template_s2je6rm",
-    //     formData,
-    //     "yuvrajsinh8418@gmail.com"
-    //   )
-    //   .then(
-    //     (response) => {
-    //       console.log("SUCCESS!", response.status, response.text);
-    //       setAcknowledgment(
-    //         "Thank you for contacting me! I will get back to you soon."
-    //       );
-    //     },
-    //     (error) => {
-    //       console.log("FAILED...", error);
-    //       setAcknowledgment("Sorry, something went wrong. Please try again.");
-    //     }
-    //   );
-
     setFormData({
-      name: "",
-      email: "",
+      from_name: "",
+      from_email: "",
       message: "",
     });
   };
 
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center p-8">
-      {/* Contact Form */}
+    <div className="flex flex-col justify-center items-center p-8">
       <div className="w-full md:w-1/2 p-4">
-        <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Contact Me</h2>
         <form ref={form} onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium">Name</label>
             <input
               type="text"
-              name="name"
-              value={formData.name}
+              name="from_name"
+              value={formData.from_name}
               onChange={handleChange}
               className="mt-1 p-2 w-full border rounded-md"
               required
@@ -82,8 +70,8 @@ const ContactMe = () => {
             <label className="block text-sm font-medium">Email</label>
             <input
               type="email"
-              name="email"
-              value={formData.email}
+              name="from_email"
+              value={formData.from_email}
               onChange={handleChange}
               className="mt-1 p-2 w-full border rounded-md"
               required
@@ -107,53 +95,65 @@ const ContactMe = () => {
             Send
           </button>
         </form>
-        {acknowledgment && (
-          <p className="mt-4 text-green-500">{acknowledgment}</p>
-        )}
       </div>
 
-      {/* Social Media and Vector */}
-      <div className="w-full md:w-1/2 p-4 flex flex-col items-center">
-        <img
-          src="path/to/your/vector/image.svg"
-          alt="Contact"
-          className="w-2/3 mb-4"
-        />
-        <div className="flex space-x-4">
-          <a
-            href="https://www.facebook.com/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-800"
+      {/* <div className="w-full md:w-1/2 p-4 flex justify-center items-center mt-8"> */}
+      <ul className="flex flex-wrap justify-center gap-4 mt-4">
+        {[
+          {
+            href: "https://github.com/yuviwolvy",
+            text: "Let's build together!",
+            icon: Github,
+            color:
+              "text-[#24292e] hover:text-bgColor hover:drop-shadow-[0_1.2px_1.2px_#24292e]",
+          },
+          {
+            href: "https://leetcode.com/u/yuvrajsinh-d-chauhan/",
+            text: "Check my stat!",
+            icon: Leetcode,
+            color:
+              "text-[#f79e1b] hover:text-bgColor hover:drop-shadow-[0_1.2px_1.2px_#f79e1b]",
+          },
+          {
+            href: "https://www.linkedin.com/in/yuviwolvy",
+            text: "Let's connect!",
+            icon: LinkedinSquare,
+            color:
+              "text-[#0077b5] hover:text-bgColor hover:drop-shadow-[0_1.2px_1.2px_#0077B5]",
+          },
+          {
+            href: "https://www.instagram.com/yuviwolvy",
+            text: "Follow me!",
+            icon: Instagram,
+            color:
+              "text-[#e1306c] hover:text-bgColor hover:drop-shadow-[0_1.2px_1.2px_#e1306c]",
+          },
+          {
+            href: "/Yuvrajsinh D. Chauhan.pdf",
+            text: "Download my Resume",
+            icon: ArrowDownCircleFill,
+            color:
+              "text-[#88D66C] hover:text-bgColor hover:drop-shadow-[0_1.2px_1.2px_#88D66C]",
+            download: "Yuvrajsinh D. Chauhan.pdf",
+          },
+        ].map((item, index) => (
+          <li
+            key={index}
+            href={item.href}
+            target={item.download ? "_self" : "_blank"}
+            rel={item.download ? undefined : "noopener noreferrer"}
+            className={`${item.color} group flex items-center gap-2 w-8 h-8`}
+            download={item.download}
           >
-            <i className="fab fa-facebook fa-2x"></i>
-          </a>
-          <a
-            href="https://www.twitter.com/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-600"
-          >
-            <i className="fab fa-twitter fa-2x"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-700 hover:text-blue-900"
-          >
-            <i className="fab fa-linkedin fa-2x"></i>
-          </a>
-          <a
-            href="https://www.instagram.com/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-pink-600 hover:text-pink-800"
-          >
-            <i className="fab fa-instagram fa-2x"></i>
-          </a>
-        </div>
-      </div>
+            <item.icon className="w-8 h-8 group-hover:hidden" />
+            <span className="hidden group-hover:block group-hover:font-extrabold group-hover:text-2xl ">
+              {item.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+      {/* </div> */}
+      <ToastContainer />
     </div>
   );
 };
